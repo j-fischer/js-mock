c58088bc44198276f1dd2ea875b55eae60625ab4
Johannes Fischer <fischer.jh@gmail.com>
2015-10-21 22:57:10 -0600

    Continued initial implementation: - added ExpectationError - added _format helper function - added more functions to Mock object, also changed return value to be an evaluation function - implemented mock(), assertIfSatisfied() API functions - added more comments for future implementations.

--- src/main/javascript/js-mock.js
+++ src/main/javascript/js-mock.js
@@ -15,64 +15,128 @@
 (function () {
   
   /* constants */
-  var _ALL_CALLS = -1;
+  var _ALL_CALLS = -1; // The scope of the invocations, i.e. exactly(3).returns('foo'); means return 'foo' for all invocations
   
   /* variables */
   var _shouldMonitorMocks = false;
   var _monitoredMocks = [];
   
+  function _format() {
+    var result = arguments[0];
+    
+    var i; len = arguments.length;  
+    for (i = 1; i < len; i++) {
+      result = result.replace("{" + i + "}", arguments[i]);
+    }
+    
+    return result;
+  }
+  
  /** 
-  * This is a module. 
+  * This is a class. 
+  *
+  * @class
+  */
+  function ExpectationError(message) {
+    this.name = 'ExpectationError';
+    this.message = message || 'Unkown expectation failed.';
+    this.stack = (new Error()).stack;
+  }
+  ExpectationError.prototype = Object.create(Error.prototype);
+  ExpectationError.prototype.constructor = ExpectationError;
+  
+ /** 
+  * This is a class. 
   *
   * @class
   */
   var Mock = function (args) {
     
-    var _scope = _ALL_CALLS;
-    var _expectTotalCalls = 0;
-    
     var _name = args.name;
     
-    var _expectations = {
+    var _scope, _callCount, _expectTotalCalls, _expectations;
+    
+    function reset() {
+      _scope = _ALL_CALLS;
+      _callCount = 0;
+      _expectTotalCalls = 0;
+      _expectations = {}; // {args, returns, will, fulfilled}
+    }
+    
+    function matchArgs(expectedArgs, actualArgs) {
+      if (expectedARgs === undefined) {
+        return true;
+      }
+      
+      // match args assuming type equality
+    }
+    
+    function evalCall() {
+      if (_callCount === _expectedCallCount) {
+        throw new ExpectationError(_format("{0} already called {1} times", _name, _expectedCallCount));
+      }
+          
+      _callCount++;
+
+      //find matching expectation, throw if no expectation is found
+      
+      //execute function if applicable
+      
+      //set fulfilled
+      
+      //return value
+    }
+        
+    this.exactly = function(count) {
+      reset();
+      
+      _expectTotalCalls = count;
+    };
+      
+    this.withExactArgs = function () {
+        
+    };
+      
+    this.returns = function (arg) {
+        
+    };
+      
+    this.will = function (willArgs, func) {
       
     };
-    
-    function init() {
       
+    this.verify = function () {
+      //verify expectations
+      
+      reset();
     }
-    
-    return {
-      exactly: function(count) {
-        
-      },
+
+    /* helpers, i.e. once, twice, onFirstCall, etc */
       
-      withExactArgs: function () {
-        
-      }, 
       
-      returns: function (arg) {
-        
-      },
-      
-      will: function (willArgs, func) {
-        
-      },
-      
-      verify: function () {
-        
-      }
-      /* helpers*/
-      
-    }
+    return evalCall;  
   };
   
   
   function mock(name) {
-    // body...
+    var mock = new Mock({
+      name: name
+    });
+    
+    if (_shouldMonitorMocks) {
+      _monitoredMocks.push(mock);
+    }
+    
+    return mock;
   }
   
   function mockProperties(objName, obj) {
-    // body...
+    var result = {};
+    Object.keys(obj).foreach(function(element, index, array) {
+      result[element] = mock(objName + "." + element);
+    });
+    
+    return result;
   }  
   
   init();
@@ -91,15 +155,28 @@
      * @module js-mock
      */
     mock: function (arg1, arg2) {
-
+      if (typeof arg1 !== "string" || !arg1) {
+        throw new TypeError("The first argument must be a string");
+      }
+      
+      if (typeof arg2 === "object" && arg2 !== null) {
+        return mockProperties(arg1, arg2);
+      }
+      
+      return mock(arg1);
     },
     
     monitorMocks: function (factoryFunc) {
-      
+      if (typeof factoryFunc !== "function") {
+        throw new TypeError("The first argument must be a function");
+      }
     },
     
     assertIfSatisfied: function () {
-      
+      var i, len = _monitoredMocks.length;
+      for (i = 0; i < len; i++) {
+        _monitoredMocks[i].verify();
+      }
     }
   };
   
