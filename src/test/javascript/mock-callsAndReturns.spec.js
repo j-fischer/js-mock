8348c48df0f3a34d944a03722962af1a75dda54c
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2015-12-03 00:01:00 -0700

    Re-applied "Reorganized and renamed unit tests and test files."
    This reverts commit c12e2d9283a0ec038c509e002b912e498945de17.

--- src/test/javascript/mock-callsAndReturns.spec.js
+++ src/test/javascript/mock-callsAndReturns.spec.js
@@ -1,87 +1,87 @@
-describe('JsMock', function(){
-  
+describe('Mock', function(){
+
   var _myFunc;
-  
+
   JsMock.monitorMocks(function () {
     _myFunc = JsMock.mock("WithExactArgs.myFunc");
   });
-  
+
   afterEach(function () {
     JsMock.assertIfSatisfied();
   });
-  
+
   /*
    * HELPER FUNCTIONS
    */
   function expectExpectationError(func, expectedErrorMsg) {
     expect(func).toThrowError(JsMock.ExpectationError, expectedErrorMsg);
   }
-  
+
   /*
    * TESTS
    */
   describe('callsAndReturns', function(){
-    
+
     it("should throw if argument is not a function", function () {
       expect(function () {
         _myFunc.callsAndReturns({});
       }).toThrowError(TypeError, "Argument must be a function");
-    }); 
-    
+    });
+
     it("should throw if no calls have been expected", function () {
       expect(function () {
         _myFunc.callsAndReturns(function () {
           throw "Should not be registered.";
         });
       }).toThrowError(Error, "You must call allowing, exactly, never, once, twice or thrice before setting any other expectations for this mock.");
-    }); 
-    
+    });
+
     it("should throw AssertionError if function throws an exception", function () {
       _myFunc.once().callsAndReturns(function () {
         throw "some error";
       });
-      
+
       expectExpectationError(_myFunc, "ExpectationError: Registered action for 'WithExactArgs.myFunc' threw an error: \"some error\".");
-		});
-    
+    });
+
     it("should pass the arguments to the function", function () {
       var arg1 = "abc";
       var arg2 = { a: "propertyVBalue" };
-      
+
       _myFunc.once().callsAndReturns(function (actualArg1, actualArg2) {
         expect(actualArg1).toBe(arg1);
         expect(actualArg2).toBe(arg2);
       });
-      
+
       _myFunc(arg1, arg2);
-		});
-    
+    });
+
     it("should do nothing with the return value", function () {
       _myFunc.once().callsAndReturns(function () {
         return "foo";
       });
-      
+
       expect(_myFunc.returns).toThrowError(Error, "callsAndReturns() is already set for this expectation. You can only define one of those two functions for you mock");
-		});
+    });
   });
-  
+
   describe('will', function(){
     it("should throw if argument is not a function", function () {
       expect(function () {
         _myFunc.will("do something");
       }).toThrowError(TypeError, "Argument must be a function");
-    }); 
-    
+    });
+
     it("should pass the arguments to the function", function () {
       var arg1 = "abc";
       var arg2 = { a: "propertyVBalue" };
-      
+
       _myFunc.once().will(function (actualArg1, actualArg2) {
         expect(actualArg1).toBe(arg1);
         expect(actualArg2).toBe(arg2);
       });
-      
+
       _myFunc(arg1, arg2);
-		});
+    });
   });
 });
--- src/test/javascript/mock-verify.spec.js
+++ src/test/javascript/mock-verify.spec.js
@@ -1,47 +1,47 @@
-describe('JsMock', function(){
-  
+describe('Mock', function(){
+
   var _myFunc;
-  
+
   JsMock.monitorMocks(function () {
     _myFunc = JsMock.mock("Verify.myFunc");
   });
-  
+
   afterEach(function () {
     JsMock.assertIfSatisfied();
   });
-  
+
   /*
    * HELPER FUNCTIONS
    */
   function expectMissingInvocationError(expectedErrorMsg) {
     expect(_myFunc.verify).toThrowError(JsMock.ExpectationError, expectedErrorMsg);
   }
-  
+
   /*
    * TESTS
    */
   describe('verify', function() {
     it("should return 'true' if all expectations pass", function () {
-      
+
       _myFunc.once();
-      
+
       _myFunc();
-      
+
       expect(_myFunc.verify()).toBe(true);
-		});
-    
+    });
+
     it("should fail a single expectation", function () {
       _myFunc.once();
-      
+
       expectMissingInvocationError('ExpectationError: Missing invocations for Verify.myFunc: ["Expectation for call 1 with args undefined, will return undefined."].');
-		});
-    
+    });
+
     it("should list all failing expectations", function () {
       _myFunc.twice();
       _myFunc.onFirstCall().withExactArgs("foo").returns("bar");
       _myFunc.onSecondCall().withExactArgs(1).returns(2);
-      
+
       expectMissingInvocationError('ExpectationError: Missing invocations for Verify.myFunc: ["Expectation for call 1 with args [\\"foo\\"], will return \\"bar\\".","Expectation for call 2 with args [1], will return 2."].');
-		});
+    });
   });
 });
