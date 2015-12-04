43fc480be138ecb7ed2ed36d3ef5f2e8711b9873
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2015-12-03 20:28:54 -0700

    Added tests for GlobalMock.expect

---
+++ src/test/javascript/globalMock-expect.spec.js
@@ -0,0 +1,54 @@
+describe('GlobalMock', function(){
+
+  var _jQueryMock;
+
+  beforeEach(function () {
+    JsMock.monitorMocks(function () {
+      _jQueryMock = JsMock.mockGlobal("$");
+    });
+  });
+
+  afterEach(JsMock.assertIfSatisfied);
+
+  /*
+   * HELPER FUNCTIONS
+   */
+  function expectExpectationError(func, expectedErrorMsg) {
+    expect(func).toThrowError(JsMock.ExpectationError, expectedErrorMsg);
+  }
+
+  /*
+   * TESTS
+   */
+  describe('expect', function(){
+
+    it("should throw if not activated", function() {
+      expect(function () {
+        _jQueryMock.expect().once();
+      }).toThrowError(Error, "Mock object has not been activated");
+    });
+
+    it("should allow to mock the function if the global object is one", function () {
+      _jQueryMock.activate();
+
+      _jQueryMock.expect().once().with("div");
+
+      expectExpectationError(function () {
+        $("span");
+      }, "ExpectationError: Unexpected invocation of '$' for call 1: actual arguments: [\"span\"].");
+
+      $("div");
+    });
+
+     it("should allow to mock any property", function () {
+      var target = {};
+      var source = {};
+
+      _jQueryMock.activate();
+
+      _jQueryMock.expect().extend.once().with(target, source).returns(target);
+
+      expect($.extend(target, source)).toBe(target);
+    });
+  });
+});
