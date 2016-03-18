98282f24de6be97e888cbd8276aab3261b8fd770
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-03-17 20:45:31 -0600

    Updated artifacts for version 0.12.0

--- dist/js-mock.js
+++ dist/js-mock.js
@@ -246,7 +246,7 @@
       });
 
       if (verificationErrors.length > 0) {
-        throw new ExpectationError(__format("Missing invocations for {0}: {1}.", _propertyName, JSON.stringify(verificationErrors))); //TODO: improve message format
+        throw new ExpectationError(__format("Missing invocations detected for global mock {0}:\n{1}", _propertyName, verificationErrors.join("\n")));
       }
 
       return true;
@@ -717,14 +717,13 @@
       Object.keys(_expectations).forEach(function(index) {
         var expectation = _expectations[index];
 
-        // TODO: Verify if this code branch can ever be executed. I think missing expectations would always fail in line 249.
         if (!expectation.fulfilled) {
-          unfulfilledExpectations.push(__format("Expectation for call {0} with args {1}, will return {2}.", index, JSON.stringify(expectation.args), JSON.stringify(expectation.returnValue)));
+          unfulfilledExpectations.push(__format("Expectation for call {0} with args {1}, will return {2}", index, (JSON.stringify(expectation.args) || "undefined").replace('\\"', '"'), JSON.stringify(expectation.returnValue)));
         }
       });
 
       if (unfulfilledExpectations.length > 0) {
-        throw new ExpectationError(__format("Missing invocations for {0}: {1}.", _name, JSON.stringify(unfulfilledExpectations))); //TODO: improve message format
+        throw new ExpectationError(__format("Missing invocations for {0}():\n>>> {1}", _name, unfulfilledExpectations.join("\n>>> ")));
       }
 
       reset();
@@ -979,6 +978,10 @@
     /* For internal debugging purposes */
     __enableLogs: function() {
       _logsEnabled = true;
+    },
+
+    __disableLogs: function() {
+      _logsEnabled = false;
     }
   };
 
