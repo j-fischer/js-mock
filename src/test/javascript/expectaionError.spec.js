edeb5b16fa7336cdb89774d366fe5b66dc39bb22
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-03-17 20:32:57 -0600

    Added test for expecation class behaviour

---
+++ src/test/javascript/expectaionError.spec.js
@@ -0,0 +1,13 @@
+describe('ExpectationError', function(){
+  /*
+   * TESTS
+   */
+  describe('constructor', function() {
+
+    it("uses default message if message is undefined", function () {
+      var error = new JsMock.ExpectationError();
+
+      expect(error.message).toBe('ExpectationError: Unknown expectation failed.');
+    });
+  });
+});
